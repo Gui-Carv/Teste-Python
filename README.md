1338ae77f5598cc15380945be5ee910b36c952e2
gui-carv <gui.20031603@gmail.com>
2024-09-10 15:03:15 -0300

    Teste finalizado

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Teste-Python
+
+## Explicação para as respostas:
+
+### 1- usando o json abaixo, retire somente os produtos em que o preço seja maior do que 30 Reais 
+
+Eu percorri cada loja e filtrei os produtos dessa loja usando uma list comprehension. Escolhi essa abordagem porque é simples e direta, e permite acessar e manipular rapidamente os dados.
+
+### 2- Use o JSON abaixo para capturar o preço do produto B
+
+Usei a função next com uma expressão geradora para obter o preço do Produto B. Essa solução é eficiente porque percorre a lista de produtos e retorna o valor assim que encontra o item desejado.
+
+### 3- Ordene a lista abaixo em ordem crescente
+
+Utilize sorted para ordenar a lista em ordem crescente. Essa função cria uma nova lista ordenada sem modificar a original.
+
+### 4- Retire todos os espaços em branco, crie uma nova lista e adicione esses itens nela
+
+Usei strip em uma list comprehension para remover os espaços em branco, resultando em uma nova lista sem espaços extras.
+
+### 5- Retire o segundo item dessa lista e imprima ela
+
+Usei pop(1) para remover o segundo item da lista.
+
+### 6- Substitua todos os "joao" da lista por "maria"
+
+Usei uma list comprehension com uma expressão condicional para substituir o nome, assim consegui criar uma lista nova onde todo nome joão é alterado por maria.
+
+### 7- Crie um loop while em Python que itera sobre os itens de uma lista e imprime os itens enquanto o valor de uma variável é menor ou igual a 5. Após imprimir cada item, o valor da variável é incrementado em 1
+
+Infelizmente não compreendi muito bem a questão e não consegui realizar. 
+
+### 8- Usando a biblioteca requests, faça uma requisição http para o endpoint https://jsonplaceholder.typicode.com/todos. Cada objeto dentro do json possui a chave "completed", que indica se a task foi completada ou não. Faça uma função que trate a resposta e retorne a quantidade de tasks completadas, e a quantidade de tasks pendentes
+
+Usei requests para fazer uma requisição HTTP e depois processei a resposta JSON para contar tasks completadas e pendentes.
+
+### 9- Faça uma requisição em uma API https://jsonplaceholder.typicode.com/users e com os dados retornados faça um parsing de dados e retire o nome, username, email, rua, numero
+
+Novamente usei o requests para uma requisição HTTP para obter dados de usuários e usei uma list comprehension para extrair nome, username, email, rua e número.
+
+### 10- Crie uma lista e adicione um item novo a ela utilizando a metodologia FIFO
+
+Criei uma lista e adicionei um item novo usando o método append. É um método adequado para implementar a abordagem FIFO, pois adiciona o item ao final da lista, preservando a ordem de inserção.
+
+### 11- Crie uma lista e adicione um item novo a ela utilizando a metodologia LIFO
+
+Criei uma lista e adicionei um item usando append, e depois removi com pop. Representando a abordagem LIFO (Last In, First Out), onde o último item adicionado é o primeiro a ser removido.
---
+++ teste_back_end_nivel_1.py
@@ -0,0 +1,198 @@
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#TESTE DE BACKEND NIVEL 1 - GRUPO PEGAZUS
+
+#Faça o teste abaixo 100% sozinho, sem a ajuda de CHAT GPT, amigos, familiares, professores ou etc.. conseguimos facilmente identificar
+
+#lembre-se de detalhar as respostas, assim conseguimos analisar ainda mais o seu conhecimento tecnico
+
+#caso prefira, pode fazer o desafio em outro arquivo separado, e só colar a solução completa abaixo de cada exercicio
+#////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#TODO- 1- usando o json abaixo, retire somente os produtos em que o preço seja maior do que 30 Reais 
+#Explique detalhadamente por que voce decidiu essa solução e não outra
+
+response = [
+    {
+        "nome": "Loja Exemplo 1",
+        "endereço": {
+            "rua": "Rua Exemplo 1",
+            "cidade": "Exemplo City 1"
+        },
+        "produtos": [
+            {"id": 1, "nome": "Produto A", "preço": 29.99},
+            {"id": 2, "nome": "Produto B", "preço": 34.99}
+        ]
+    },
+    {
+        "nome": "Loja Exemplo 2",
+        "endereço": {
+            "rua": "Rua Exemplo 2",
+            "cidade": "Exemplo City 2"
+        },
+        "produtos": [
+            {"id": 1, "nome": "Produto C", "preço": 45.50},
+            {"id": 2, "nome": "Produto D", "preço": 15.00}
+        ]
+    },
+    {
+        "nome": "Loja Exemplo 3",
+        "endereço": {
+            "rua": "Rua Exemplo 3",
+            "cidade": "Exemplo City 3"
+        },
+        "produtos": [
+            {"id": 1, "nome": "Produto E", "preço": 22.00},
+            {"id": 2, "nome": "Produto F", "preço": 39.99}
+        ]
+    },
+    {
+        "nome": "Loja Exemplo 4",
+        "endereço": {
+            "rua": "Rua Exemplo 4",
+            "cidade": "Exemplo City 4"
+        },
+        "produtos": [
+            {"id": 1, "nome": "Produto G", "preço": 55.00},
+            {"id": 2, "nome": "Produto H", "preço": 5.99}
+        ]
+    },
+    {
+        "nome": "Loja Exemplo 5",
+        "endereço": {
+            "rua": "Rua Exemplo 5",
+            "cidade": "Exemplo City 5"
+        },
+        "produtos": [
+            {"id": 1, "nome": "Produto I", "preço": 33.00},
+            {"id": 2, "nome": "Produto J", "preço": 27.50}
+        ]
+    }
+]
+
+#Resposta:
+# produtos_acima_30 = [
+#     {
+#         "nome": loja["nome"],
+#         "endereço": loja["endereço"],
+#         "produtos": [produto for produto in loja["produtos"] if produto["preço"] <= 30]
+#     }
+#     for loja in response
+# ]
+# print(produtos_acima_30)
+
+#TODO- 2-Use o JSON abaixo para capturar o preço do produto B
+#explique detalhadamente por que escolheu essa solução e não outra
+
+responsejson = {
+    "nome": "Loja Exemplo",
+    "endereço": {
+        "rua": "Rua Exemplo",
+        "cidade": "Exemplo City"
+    },
+    "produtos": [
+        {"id": 1, "nome": "Produto A", "preço": 29.99},
+        {"id": 2, "nome": "Produto B", "preço": 19.99}
+    ]
+}
+
+#Resposta:
+# preco_produto_b = next(produto["preço"] for produto in responsejson["produtos"] if produto["nome"] == "Produto B")
+# print("Produto B: ", preco_produto_b)
+
+
+#TODO- 3- Ordene a lista abaixo em ordem crescente
+#explique detalhadamente por que escolheu essa solução e não outra
+
+lista = [5,8,3,0,8,1,9,10,20,30]
+
+#Resposta:
+# lista_ordenada = sorted(lista)
+# print(lista_ordenada)
+
+
+#TODO- 4-Retire todos os espaços em branco, crie uma nova lista e adicione esses itens nela
+
+
+lista = ["   joao   ","   maria   ","  joana  "]
+
+#Resposta:
+# lista_sem_espacos = [nome.strip() for nome in lista]
+# print(lista_sem_espacos)
+
+#TODO- 5-Retire o segundo item dessa lista e imprima ela:
+
+lista = [1,2,3,4,5,6]
+
+#Resposta:
+# lista.pop(1)
+# print(lista)
+
+#TODO- 6-substitua todos os "joao" da lista por "maria"
+
+lista = ["joao", "ana", "joana","joao", "ricardo", "joao"]
+
+#Resposta:
+# nova_lista = ["maria" if nome == "joao" else nome for nome in lista]
+# print(nova_lista)
+
+#TODO- 7-criar um loop while em Python que itera sobre os itens de uma lista e imprime os itens enquanto o valor de uma variável é menor ou igual a 5. Após imprimir cada item, o valor da variável é incrementado em 1
+#explique detalhadamente por que escolheu essa solução e não outra
+
+
+
+#TODO- 8-usando a biblioteca requests, faça uma requisição http para o endpoint https://jsonplaceholder.typicode.com/todos. cada objeto dentro do json possui a chave "completed". que indica se a task foi completada ou não. Faça uma função que trate a resposta e retorne a quantidade de tasks completadas, e a quantidade de tasks pendentes
+#explique detalhadamente por que escolheu essa solução e não outra
+
+#Resposta:
+# import requests
+# def contar_tasks():
+#     response = requests.get('https://jsonplaceholder.typicode.com/todos')
+#     tasks = response.json()
+#     completadas = sum(1 for task in tasks if task['completed'])
+#     pendentes = len(tasks) - completadas
+#     return completadas, pendentes
+# print(contar_tasks())
+
+#TODO- 9-faça uma requisição em uma API  https://jsonplaceholder.typicode.com/users e com os dados retornados 
+# faça um parsing de dados e retire  o nome, username, email, rua, numero
+#explique detalhadamente por que escolheu essa solução e não outra
+
+#Resposta:
+# import requests
+# def obter_dados_usuarios():
+#     response = requests.get('https://jsonplaceholder.typicode.com/users')
+#     usuarios = response.json()
+#     dados_extraidos = [
+#         {
+#             "nome": usuario["name"],
+#             "username": usuario["username"],
+#             "email": usuario["email"],
+#             "rua": usuario["address"]["street"],
+#             "numero": usuario["address"]["suite"]
+#         }
+#         for usuario in usuarios
+#     ]
+#     return dados_extraidos
+# print(obter_dados_usuarios())
+
+
+#TODO-10-crie uma lista e adicione um item novo a ela utilizando a metodologia FIFO
+
+#Resposta:
+# lista_fifo = []
+# lista_fifo.append("Novo item")
+# print(lista_fifo)
+
+#TODO- 11-crie uma lista e adicione um item novo a ela utilizando a metodolofia LIFO
+
+#Resposta:
+# lista_lifo = []
+# lista_lifo.append("Novo item")
+# print(lista_lifo.pop()) 
+
+
+#TODO- DESAFIO!!
+
+#crie uma interface de banco, o programa deve utilizar POO, a classe deve ter os atributos id, nome, cpf e saldo , aonde o saldo deve ser iniciado em 0, e o id deve ser autoicremental. a interfaçe deve permitir a criação de uma conta, e a realização das operações de saque e deposito do saldo da conta
+
